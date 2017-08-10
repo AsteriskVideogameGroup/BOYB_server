8bf0ac18b5501a2f48c08be85ab8e249ce7c1c80
castogio <gioacchino.castorio@icloud.com>
2017-08-10 07:24:00 +0200

    file di inizializzazione dati database

--- servermain.py
+++ servermain.py
@@ -1,50 +1,25 @@
 from control.gamemanageusecase.matchmakinghandler import MatchMakingHandler
 from foundations.dao.idaoabstractfactory import IDAOAbstractFactory
-from foundations.geometry.shapedimension import Dimension
 from foundations.inversionofcontrol.dicontainer import DepInjContainer
 from foundations.network.corba.corbamanagerfactory import CorbaManagerFactory
 from foundations.network.corba.icorbamanager import ICorbaManager
-from model.gamemanageusecase.modes.mode import GameMode
 
 if __name__ == "__main__":
 
+    # inizializzazione container IoC
     container: DepInjContainer = DepInjContainer().init("etc/config.json")
 
-    factory: CorbaManagerFactory = container.getObject("corbamangerfactory")
-
-    corba: ICorbaManager = factory.getCorbaManager()
-    corba.init()
-
-    #ClientsRegister().init(corba)
-    #clientregister: ClientsRegister = container.getObject("clientregister")
-    #clientregister.init()
-    #print(clientregister.corbamanager)
-
-    matchmakinghandler: MatchMakingHandler = container.getObject("matchmakinghandler")
-
-    print(matchmakinghandler.clientsregister)
-    print(matchmakinghandler.matchmakingfactory)
+    # inizializzazione comunicazione di rete CORBA
+    corbafactory: CorbaManagerFactory = container.getObject("corbamangerfactory")
+    corbamanger: ICorbaManager = corbafactory.getCorbaManager()
+    corbamanger.init()
 
     # inizializzazione database
-    daof: IDAOAbstractFactory = container.getObject("daofactory")
-    daof.init()
+    daofactory: IDAOAbstractFactory = container.getObject("daofactory")
+    daofactory.init()
 
-    '''# TODO testing
-    
-
-    mapsize: Dimension = Dimension(22, 33)
-    newmode: GameMode = GameMode("pepito", mapsize, 4, 300, "ClassicMapElementDisposalStrategy", "ClassicObjectFactory")
-
-    #print(newmode)
-    modesave = daof.getModeDAO()
-
-    #modesave.save(newmode)
-    mode = modesave.getByID("pepito")
-    print(mode)
-
-
-    #TODO testing'''
-
-
-    print(corba.remotize(matchmakinghandler, "matchmakinghandler"))
+    # inizializzazione ascoltatore di matchaking
+    handlername: str = "matchmakinghandler"
+    matchmakinghandler: MatchMakingHandler = container.getObject(handlername)
+    corbamanger.remotize(matchmakinghandler, handlername)
 
---
+++ setupdatabase.py
@@ -0,0 +1,46 @@
+from foundations.dao.idaoabstractfactory import IDAOAbstractFactory
+from foundations.dao.imodedao import IModeDAO
+from foundations.dao.iplayerdao import IPlayerDAO
+from foundations.geometry.shapedimension import Dimension
+from foundations.inversionofcontrol.dicontainer import DepInjContainer
+from model.gamemanageusecase.modes.mode import GameMode
+from model.gamemanageusecase.players.player import Player
+
+if __name__ == "__main__":
+    # inizializzazione container IoC
+    container: DepInjContainer = DepInjContainer().init("etc/config.json")
+
+    # inizializzazione database
+    daofactory: IDAOAbstractFactory = container.getObject("daofactory")
+    daofactory.init()
+
+    # creazione oggetti player da salvare nel DB
+    player1: Player = Player("p1")
+    player2: Player = Player("p2")
+    player3: Player = Player("p3")
+    player4: Player = Player("p4")
+
+    # creazione oggetti mode da salvare nel DB
+    name: str = "classic_mode"
+    mapdimensions: Dimension = Dimension(5, 5)
+    maxplayers: int = 4
+    secgameduration: int = 300
+    mapelementdisposerid: str = "ClassicMapElementDisposalStrategy"
+    objectfactoryid: str = "ClassicObjectFactory"
+    mode: GameMode = GameMode(name, mapdimensions, maxplayers, secgameduration, mapelementdisposerid, objectfactoryid)
+
+    # data access objects
+    playerdao: IPlayerDAO = daofactory.getPlayerDAO()
+    modedao: IModeDAO = daofactory.getModeDAO()
+
+    # salvataggio nel database dei player
+    playerdao.save(player1)
+    playerdao.save(player2)
+    playerdao.save(player3)
+    playerdao.save(player4)
+
+    # salvataggio nel database della modalità
+    modedao.save(mode)
+
+
+
